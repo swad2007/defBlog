7a6de08eb59d14226ef47917eb6051ac6adc8bbd
FlyLive <3888@list.ru>
2020-04-09 13:11:01 +0300

    add req

--- defBlog/defBlog/settings.py
+++ defBlog/defBlog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c9xtd5xczn4e0fdl0jx%sfu6ag!0zv7*egx=tmhzjyhxo#jm!i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
